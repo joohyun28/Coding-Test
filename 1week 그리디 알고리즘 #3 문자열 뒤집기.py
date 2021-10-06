8631eb4676759b74ee1b28895abd580688577197
joohyun01 <63608587+joohyun28@users.noreply.github.com>
2021-10-06 17:36:07 +0900

    Update 1week 그리디 알고리즘 #3 문자열 뒤집기.py

--- 1week 그리디 알고리즘 #3 문자열 뒤집기.py
+++ 1week 그리디 알고리즘 #3 문자열 뒤집기.py
@@ -10,10 +10,10 @@ else:
     count0 += 1   #1이면 count0++
 
 for i in range(len(s) - 1):  #첫 번째 수를 제외한 범위에서
-    if s[i] != s[i+1]:       #두 번째 수와 세 번째 수가 다르고
-        if s[i+1] =='1':     #세 번째 수가 1이라면
+    if s[i] != s[i+1]:       #n번째 수와 n+1번째 수가 다르고
+        if s[i+1] =='1':     #n+1번째 수가 1이라면
             count0 += 1      #count0++
-        else:                #세 번째 수가 0이라면
+        else:                #n+1번째 수가 0이라면
             count1 += 1      #count1++
             
 print(min(count0, count1))   #count의 최솟값 출력
