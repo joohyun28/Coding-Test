1d0836ecd52dc8d1eff56727d96a7c853d96a26e
joohyun01 <63608587+joohyun28@users.noreply.github.com>
2021-10-06 17:34:53 +0900

    Update 1week 그리디 알고리즘 #3 문자열 뒤집기.py

--- 1week 그리디 알고리즘 #3 문자열 뒤집기.py
+++ 1week 그리디 알고리즘 #3 문자열 뒤집기.py
@@ -1,20 +1,20 @@
 s = input()
 
-count1 = 0
-count0 = 0
+count1 = 0        #1로 바꾸는 경우의 수
+count0 = 0        #0으로 바꾸는 경우의 수
 
-if s[0] == 0:
-    count1 += 1
+if s[0] == 0:     #만약 첫 번째 수가 0이라면
+    count1 += 1   #count1++
     
 else:
-    count0 += 1
+    count0 += 1   #1이면 count0++
 
-for i in range(len(s) - 1):
-    if s[i] != s[i+1]:
-        if s[i+1] =='1':
-            count0 += 1
-        else:
-            count1 += 1
-
-print(min(count0, count1))
+for i in range(len(s) - 1):  #첫 번째 수를 제외한 범위에서
+    if s[i] != s[i+1]:       #두 번째 수와 세 번째 수가 다르고
+        if s[i+1] =='1':     #세 번째 수가 1이라면
+            count0 += 1      #count0++
+        else:                #세 번째 수가 0이라면
+            count1 += 1      #count1++
+            
+print(min(count0, count1))   #count의 최솟값 출력
     
