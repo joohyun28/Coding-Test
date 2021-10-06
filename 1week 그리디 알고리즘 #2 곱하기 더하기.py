b4b15e0c706b64b8bcc7a6d3be5e7936be44dc8a
joohyun28 <63608587+joohyun28@users.noreply.github.com>
2021-10-06 16:16:43 +0900

    Update 1week 그리디 알고리즘 #2 곱하기 더하기.py

--- 1week 그리디 알고리즘 #2 곱하기 더하기.py
+++ 1week 그리디 알고리즘 #2 곱하기 더하기.py
@@ -2,11 +2,13 @@ S = input()
 
 result = int (S[0])
 
-for i in range(1, len(S)):
+#i의 범위 설정 1 <= i <= S길이
+for i in range(1, len(S)): 
+    #두 값 중 하나가 1 이하이면 덧셈 
     if int(S[i]) <=1 or result <=1:
         result = result + int(S[i])
+    #아니면 곱셈
     else:
         result = result * int(S[i])
 
 print(result)
-
